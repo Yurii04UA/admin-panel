f66a54fdc044d2932b0c3d8cf003c1cc8d2a8774
Yurii04UA <uyriparaka@gmail.com>
2022-09-20 21:22:16 +0300

    feat: added input

--- src/Components/Auth/Form/Input/Input.tsx
+++ src/Components/Auth/Form/Input/Input.tsx
@@ -1,18 +1,38 @@
 import React from "react";
+import "./Input.scss";
 
-export const Input = () => {
+interface IInput {
+  label: string;
+  type: string;
+  placeholder: string;
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+  icon:boolean;
+}
+
+export const Input = ({
+      label,
+      type,
+      placeholder,
+      value,
+      setValue,
+      icon
+}: IInput) => {
   return (
-   //  <label>
-   //    email
-   //    <div className="error">{emailErr ? emailErr : null}</div>
-   //    <input
-   //      type="text"
-   //      placeholder="Email address"
-   //      value={email}
-   //      onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-   //        setEmail(e.target.value)
-   //      }
-   //    />
-   //  </label>
+    <label className="label">
+      {label}
+      <input
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+          setValue(e.target.value)
+        }
+      />
+      {icon && 
+         <button type="button" className="icon">
+         a
+         </button>}
+    </label>
   );
 };
--- src/Pages/Login/Login.tsx
+++ src/Pages/Login/Login.tsx
@@ -1,10 +1,39 @@
+import { useState } from "react";
 import AuthWindow from "../../Components/Auth/AuthWindow/AuthWindow";
+import { Input } from "../../Components/Auth/Form/Input/Input";
 import "./Login.scss";
 
 export const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-      <AuthWindow isFooter={true} title={{ title: "Log In to Dashboard Kit", subtitle: "Enter your email and password" }}>
+      <AuthWindow
+        isFooter={true}
+        title={{
+          title: "Log In to Dashboard Kit",
+          subtitle: "Enter your email and password",
+        }}
+      >
+        <form>
+          <Input
+            label="email"
+            type="text"
+            placeholder="Enter your email"
+            value={email}
+            setValue={setEmail}
+            icon={false}
+          />
+          <Input
+            label="password"
+            type="password"
+            placeholder="Enter your password"
+            value={password}
+            setValue={setPassword}
+            icon={true}
+          />
+          <button type="submit">sdf</button>
+        </form>
       </AuthWindow>
     </>
   );
