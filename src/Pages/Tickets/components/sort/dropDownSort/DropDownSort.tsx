e032671be3f2ca604b34530b748ba603551cbefe
Yurii04UA <uyriparaka@gmail.com>
2022-10-05 23:24:42 +0300

    feat: i all broke

--- src/Pages/Tickets/components/sort/dropDownSort/DropDownSort.tsx
+++ src/Pages/Tickets/components/sort/dropDownSort/DropDownSort.tsx
@@ -1,7 +1,49 @@
-import s from './DropDownSort.module.scss';
+import { useState,useEffect } from "react";
+
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+
+import s from "./DropDownSort.module.scss";
 
 export const DropDownSort = () => {
+  const button = ["Details", "Name", "Date", "Priority"];
+  const [aa,setAa] = useState('');
   return (
-   <div className={s.dropDown}>Sort</div>
+    <div className={s.dropDown}>
+      {button.map((btn) => {
+        
+        const [sort, setSort] = useState<"none" | "up" | "down">('none');
+       
+        const changeArrow = () => {
+          setAa(btn);
+          if(aa === btn){
+            setSort('down');
+          }
+          
+          
+        };
+        
+        useEffect(() =>{
+          setSort('none');
+          if(aa === btn){
+            setSort('up');
+          }
+          
+        },[aa]);
+
+        return (
+          <button onClick={changeArrow} key={btn}>
+            {btn}
+            {sort === "none" ? (
+              ""
+            ) : "" || sort === "up" ? (
+              <ArrowUpwardIcon />
+            ) : null || sort === "down" ? (
+              <ArrowDownwardIcon />
+            ) : null}
+          </button>
+        );
+      })}
+    </div>
   );
 };
